35a258b2273fe0be2b779acc05fba4035ef70409
Pete Montgomery <sumo.the.cat@gmail.com>
2013-07-16 13:38:45 +0100

    simple implementation of UK Gemini model

--- Catalogue.Data/Model/Item.cs
+++ Catalogue.Data/Model/Item.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Catalogue.Data.Model
 {
@@ -11,16 +9,42 @@ namespace Catalogue.Data.Model
 
         public string   Xml      { get; set; }
         public Metadata Metadata { get; set; }
+        public bool     Public   { get; set; }
 
         public string   Location { get; set; }
         public bool     TopCopy  { get; set; }
     }
 
 
+    /// <summary>
+    /// A simple implementation of the UK Gemini 2.2 metadata standard.
+    /// </summary>
     public class Metadata
     {
         public string Title { get; set; }
+        public string Abstract { get; set; }
+        public string TopicCategory { get; set; }
+        public List<string> Keywords { get; set; }
+        public string TemporalExtent { get; set; }
+        public string DatasetReferenceDate { get; set; }
+        public string Lineage { get; set; }
         public string DataFormat { get; set; }
-        public string BoundingBox { get; set; }
+        public string ResponsibleOrganisation { get; set; }
+        public string LimitationsOnPublicAccess { get; set; }
+        public string UseConstraints { get; set; }
+        public string MetadataDate { get; set; }
+        public string MetadataLanguage { get; set; }
+        public string MetadataPointOfContact { get; set; }
+        public string UniqueResourceIdentifier { get; set; }
+        public string ResourceType { get; set; }
+        public BoundingBox BoundingBox { get; set; }
+    }
+
+    public class BoundingBox
+    {
+        public string North { get; set; }
+        public string South { get; set; }
+        public string East  { get; set; }
+        public string West  { get; set; }
     }
 }
