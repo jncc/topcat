80ed58ee08635f80df01efec3d377dad331800ec
Pete Montgomery <sumo.the.cat@gmail.com>
2013-10-24 20:56:29 +0100

    ninject support for web api

--- Catalogue.Web/Injection/NinjectBootstrapping.cs
+++ Catalogue.Web/Injection/NinjectBootstrapping.cs
@@ -1,38 +1,35 @@
-
-// this nasty file was added by the Ninject.Web.Common nuget package
-// and then edited as intended, and edited further to work with asp.net web api v4
+﻿
+// this nasty file was created by the Ninject.Web.Common nuget package in the App_Start folder,
+// then moved, edited as intended, and edited further to work with asp.net web api v4
 
 
 using System.Web.Http;
+using System;
+using System.Web;
 using Catalogue.Web.Injection;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
 
-[assembly: WebActivator.PreApplicationStartMethod(typeof(Catalogue.Web.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(Catalogue.Web.App_Start.NinjectWebCommon), "Stop")]
+[assembly: WebActivator.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
 
-namespace Catalogue.Web.App_Start
+namespace Catalogue.Web.Injection
 {
-    using System;
-    using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Web.Common;
-
-    public static class NinjectWebCommon 
+    public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -40,7 +37,7 @@ namespace Catalogue.Web.App_Start
         {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -50,7 +47,7 @@ namespace Catalogue.Web.App_Start
             var kernel = new StandardKernel();
             kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
             kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
-            
+
             RegisterServices(kernel);
 
             // Install our Ninject-based IDependencyResolver into the Web API config
@@ -67,6 +64,6 @@ namespace Catalogue.Web.App_Start
         private static void RegisterServices(IKernel kernel)
         {
             kernel.Load<MainNinjectModule>();
-        }        
+        }
     }
 }
