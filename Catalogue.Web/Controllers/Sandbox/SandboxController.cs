bb3ec49bee87358ca4b5df77c5c5becd15e0c294
Pete Montgomery <sumo.the.cat@gmail.com>
2013-10-24 16:14:03 +0100

    work on windows auth

--- Catalogue.Web/Controllers/Sandbox/SandboxController.cs
+++ Catalogue.Web/Controllers/Sandbox/SandboxController.cs
@@ -1,16 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
 
 namespace Catalogue.Web.Controllers.Sandbox
 {
     public class SandboxController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Active()
         {
+            var p = HttpContext.User.Identity;
+
+
+            string username = "username";
+            string domain = "jncc-boss.green.jncc.gov.uk";
+
+
+            var domainContext = new PrincipalContext(ContextType.Domain, domain);
+            var user = UserPrincipal.FindByIdentity(domainContext, username);
+            string email = user.EmailAddress;
+
+            // Add the "proxyaddresses" entries.
+            var properties = ((DirectoryEntry)user.GetUnderlyingObject()).Properties;
+
+            var emails = from object property in properties["proxyaddresses"]
+                         select property.ToString();
+
             return Json(HttpContext.User.Identity.Name, JsonRequestBehavior.AllowGet);
         }
     }
+//
+//    public static class AccountManagementExtensions
+//    {
+//
+//        public static String GetProperty(this IPrincipal principal, String property)
+//        {
+//            DirectoryEntry directoryEntry = principal.GetUnderlyingObject() as DirectoryEntry;
+//            if (directoryEntry.Properties.Contains(property))
+//                return directoryEntry.Properties[property].Value.ToString();
+//            else
+//                return String.Empty;
+//        }
+//
+//        public static String GetCompany(this IPrincipal principal)
+//        {
+//            return principal.GetProperty("company");
+//        }
+//
+//        public static String GetDepartment(this IPrincipal principal)
+//        {
+//            return principal.GetProperty("department");
+//        }
+//
+//    }
 }
