213a87508097a0028ca288a45fcf5482d7a7f9bd
Pete Montgomery <sumo.the.cat@gmail.com>
2013-11-01 11:45:43 +0000

    versioning static resources

---
+++ Catalogue.Utilities/Versioning/VersionStamp.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Catalogue.Utilities.Versioning
+{
+    public class VersionStamp
+    {
+        public static string Stamp = GetVersionStamp();
+
+        static string GetVersionStamp()
+        {
+            return Assembly.GetExecutingAssembly()
+                       .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false)
+                       .Cast<AssemblyInformationalVersionAttribute>()
+                       .Select(a => a.InformationalVersion)
+                       .SingleOrDefault() ?? "0.0.0.0";
+        }
+    }
+}
