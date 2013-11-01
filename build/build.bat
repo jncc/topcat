484898652a6378db5eab408acc58717cc2947f2d
Pete Montgomery <sumo.the.cat@gmail.com>
2013-11-01 12:00:41 +0000

    docs

--- build/build.bat
+++ build/build.bat
@@ -1,5 +1,5 @@
 REM You can also parameterise the build script, e.g.
-REM msbuild catalogue.build.targets /p:Configuration=Release;Environment=Beta;Version=true
+REM msbuild build.targets /p:Configuration=Release;Environment=Beta;Version=true
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild catalogue.build.targets
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild build.targets
 Pause
