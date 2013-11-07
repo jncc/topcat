f0e308ac29213032bf687175657ac60095ec59f8
Pete Montgomery <sumo.the.cat@gmail.com>
2013-11-07 13:49:50 +0000

    start to try out versioning

---
+++ Catalogue.Tests/Explicit/versioning_proof_of_concept.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalogue.Tests.Utility;
+using NUnit.Framework;
+
+namespace Catalogue.Tests.Explicit
+{
+    class versioning_proof_of_concept : DatabaseTestFixture
+    {
+        [Test, Explicit]
+        public void try_out_versioning()
+        {
+        }
+    }
+}
