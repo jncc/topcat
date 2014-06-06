d102339a5209eb3f7d5fb799f2a71d0cdda847f3
Stephen Batty <sbatty@gmail.com>
2014-06-06 11:46:45 +0100

    not loggin errors from api controllers trying this http://stackoverflow.com/questions/10059563/elmah-and-api-controller-in-mvc4-not-logging-errors

--- Catalogue.Web/Global.asax.cs
+++ Catalogue.Web/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Filters;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Catalogue.Data.Import;
@@ -23,12 +24,23 @@ namespace Catalogue.Web
     public class WebApiApplication : HttpApplication
     {
         public static IDocumentStore DocumentStore { get; private set; }
+        
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterHttpFilters(HttpFilterCollection filters)
+        {
+            filters.Add(new ElmahHandledErrorLoggerFilter());
+        }
+
+        /*public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new ElmahHandledErrorLoggerFilter());
             filters.Add(new HandleErrorAttribute());
         
-        }
+        }*/
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -42,6 +54,7 @@ namespace Catalogue.Web
 
             InitializeDataStore();
             RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterHttpFilters(GlobalConfiguration.Configuration.Filters);
         }
 
         static void ConfigWebApi(HttpConfiguration config)
