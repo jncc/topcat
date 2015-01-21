119fab7f8d5b52837c9a12732e3f973839eefa71
felix mason <felix.mason@jncc.gov.uk>
2015-01-21 09:21:39 +0000

    wip

---
+++ Catalogue.Data/Import/Mappings/PubCatMapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalogue.Data.Model;
+using Catalogue.Gemini.Model;
+using CsvHelper.Configuration;
+
+namespace Catalogue.Data.Import.Mappings
+{
+    public class PubCatMapper : BaseMapper, IMapping
+    {
+        public void Apply(CsvConfiguration config)
+        {
+            config.TrimFields = true;
+            config.RegisterClassMap<RecordMap>();
+            config.RegisterClassMap<GeminiMap>();
+        }
+    }
+
+    public class GeminiMap : CsvClassMap<Metadata>
+    {
+        public override void CreateMap()
+        {
+            Map(m => m.Title).Name("Title");
+            Map(m => m.AdditionalInformationSource).Name("Author");
+
+        }
+    }
+
+
+    public class RecordMap : CsvClassMap<Record>
+    {
+        public override void CreateMap()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
