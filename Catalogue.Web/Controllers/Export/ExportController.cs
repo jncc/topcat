455db423736309ae4ad3842275c3cea2f394ea9c
Pete Montgomery <sumo.the.cat@gmail.com>
2015-02-10 11:58:42 +0000

    expose exporter as web api

--- Catalogue.Web/Controllers/Export/ExportController.cs
+++ Catalogue.Web/Controllers/Export/ExportController.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
+using Catalogue.Data.Export;
 using Catalogue.Data.Model;
 using Raven.Client;
 
@@ -19,15 +24,27 @@ namespace Catalogue.Web.Controllers.Export
             this.db = db;
         }
 
-        public List<Record> Get(string k)
+        public HttpResponseMessage Get(string k)
         {
-            var keyword = ParameterHelper.ParseKeywords(new [] { k }).Single(); // for now, support only one
+            var keyword = ParameterHelper.ParseKeywords(new[] { k }).Single(); // for now, support only one
 
-            var query = db.Query<Record>()
-                .Where(r => r.Gemini.Keywords.Any(kw => kw.Value == keyword.Value && kw.Vocab == keyword.Vocab));
+            var q = from r in db.Query<Record>()
+                    where r.Gemini.Keywords.Any(x => x.Vocab == keyword.Vocab && x.Value == keyword.Value)
+                    select r;
 
-            return query.ToList();
+            var writer = new StringWriter();
+            var exporter = new Exporter();
+            exporter.Export(q.ToList(), writer);
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK) {Content = new StringContent(writer.ToString())};
+
+            result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                {
+                    FileName = "topcat-export-" + DateTime.Now.ToString("yyyyMMdd-hhmmss") + ".tsv"
+                };
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            return result;
         }
-
     }
 }
