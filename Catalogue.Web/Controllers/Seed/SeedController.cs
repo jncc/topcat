e7586a77ac4c3de89b1c4a97eb077c817d29466c
Pete Montgomery <sumo.the.cat@gmail.com>
2015-02-12 15:35:15 +0000

    seeder for non-dev

---
+++ Catalogue.Web/Controllers/Seed/SeedController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Catalogue.Data.Seed;
+using Raven.Client;
+
+namespace Catalogue.Web.Controllers.Seed
+{
+    public class SeedController : ApiController
+    {
+        private IDocumentStore store;
+
+        public SeedController(IDocumentStore store)
+        {
+            this.store = store;
+        }
+
+        /// <summary>
+        /// Useful for non-live instances. Requires a POST so is difficult to do by accident.
+        /// </summary>
+        public void Post()
+        {
+            Seeder.Seed(store);
+        }
+    }
+}
