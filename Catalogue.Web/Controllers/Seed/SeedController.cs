49a9c78a66b4715de0c04cf6282af64324737e5c
Pete Montgomery <sumo.the.cat@gmail.com>
2015-02-13 11:24:05 +0000

    seed controller for testing

--- Catalogue.Web/Controllers/Seed/SeedController.cs
+++ Catalogue.Web/Controllers/Seed/SeedController.cs
@@ -1,28 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 using Catalogue.Data.Seed;
+using Catalogue.Web.Code;
 using Raven.Client;
 
 namespace Catalogue.Web.Controllers.Seed
 {
     public class SeedController : ApiController
     {
-        private IDocumentStore store;
+        readonly IDocumentStore store;
+        readonly IEnvironment environment;
 
-        public SeedController(IDocumentStore store)
+        public SeedController(IDocumentStore store, IEnvironment environment)
         {
             this.store = store;
+            this.environment = environment;
         }
 
         /// <summary>
         /// Useful for non-live instances. Requires a POST so is difficult to do by accident.
         /// </summary>
-        public void Post()
+        public HttpResponseMessage Post()
         {
+            if (environment.Name == "Live")
+                throw new InvalidOperationException("Oops, you surely didn't mean to seed the live instance..?");
+
             Seeder.Seed(store);
+
+            return new HttpResponseMessage();
         }
     }
 }
--- Catalogue.Web/Customization/CustomWebApiAssembliesResolver.cs
+++ Catalogue.Web/Customization/CustomWebApiAssembliesResolver.cs
@@ -8,9 +8,8 @@ using System.Web.Http.Dispatcher;
 namespace Catalogue.Web.Customization
 {
     /// <summary>
-    /// Restricts the assemblies considered when webapi looks for controllers.
-    /// This is to workaround the problem of controllers/routes clashing with RavenDB
-    /// which also uses webapi.
+    /// Restricts the assemblies available to webapi when searching for controllers to just this assembly.
+    /// Avoids clashing with controllers in RavenDB assemblies.
     /// </summary>
     public class CustomWebApiAssembliesResolver : DefaultAssembliesResolver
     {
