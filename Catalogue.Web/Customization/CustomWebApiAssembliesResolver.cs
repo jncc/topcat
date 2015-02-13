9aa214b99317bf2cb4fb7d71c0ecee9218fc6b7c
Pete Montgomery <sumo.the.cat@gmail.com>
2015-02-13 10:41:39 +0000

    workaround greedy webapi controller resoluton

---
+++ Catalogue.Web/Customization/CustomWebApiAssembliesResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http.Dispatcher;
+
+namespace Catalogue.Web.Customization
+{
+    /// <summary>
+    /// Restricts the assemblies considered when webapi looks for controllers.
+    /// This is to workaround the problem of controllers/routes clashing with RavenDB
+    /// which also uses webapi.
+    /// </summary>
+    public class CustomWebApiAssembliesResolver : DefaultAssembliesResolver
+    {
+        public override ICollection<Assembly> GetAssemblies()
+        {
+            return new [] { typeof(CustomWebApiAssembliesResolver).Assembly };
+        }
+    }
+}
