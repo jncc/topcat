0c72c1dbd0729a00d6635b1ac7b9d2b19ecd3f61
Pete Montgomery <sumo.the.cat@gmail.com>
2015-03-16 16:40:16 +0000

    management dump

---
+++ Catalogue.Web/Controllers/Dumps/DumpsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Catalogue.Data.Indexes;
+using Raven.Client;
+
+namespace Catalogue.Web.Controllers.Dumps
+{
+    public class DumpsController : ApiController
+    {
+        readonly IDocumentSession db;
+
+        public DumpsController(IDocumentSession db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet, Route("api/dumps/allkeywordsinrecords")]
+        public List<Tuple<string, string>> AllKeywordsInRecords()
+        {
+            var q = db.Query<RecordKeywordIndex.Result, RecordKeywordIndex>();
+
+            return q.ToList().Select(k => Tuple.Create(k.Vocab, k.Value)).ToList();
+        }
+    }
+}
