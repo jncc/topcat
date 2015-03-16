d3dcbf0a9e1487d83ee8644a201804e532f59a63
Pete Montgomery <sumo.the.cat@gmail.com>
2015-03-16 16:51:37 +0000

    management dump 2

--- Catalogue.Web/Controllers/Dumps/DumpsController.cs
+++ Catalogue.Web/Controllers/Dumps/DumpsController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Catalogue.Data.Indexes;
+using Catalogue.Gemini.Model;
 using Raven.Client;
 
 namespace Catalogue.Web.Controllers.Dumps
@@ -19,11 +20,14 @@ namespace Catalogue.Web.Controllers.Dumps
         }
 
         [HttpGet, Route("api/dumps/allkeywordsinrecords")]
-        public List<Tuple<string, string>> AllKeywordsInRecords()
+        public List<MetadataKeyword> AllKeywordsInRecords()
         {
             var q = db.Query<RecordKeywordIndex.Result, RecordKeywordIndex>();
 
-            return q.ToList().Select(k => Tuple.Create(k.Vocab, k.Value)).ToList();
+            var result = q.Take(1024).ToList();
+            if (result.Count == 1024) throw new Exception("Too many results. Needs to page them!");
+            
+            return result.Select(k => new MetadataKeyword { Vocab = k.Vocab, Value = k.Value }).ToList();
         }
     }
 }
