1ecbd0c32e034ff9b3cdc9a2348a587b921b1398
felix mason <felix.mason@jncc.gov.uk>
2015-04-09 16:21:11 +0100

    #104 Keywords are not duplicated

--- Catalogue.Data/Write/VocabularyService.cs
+++ Catalogue.Data/Write/VocabularyService.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Catalogue.Gemini.Model;
 using Catalogue.Utilities.Text;
 using Moq;
@@ -87,7 +89,11 @@ namespace Catalogue.Data.Write
 
         public VocabularyServiceResult Upsert(Vocabulary vocab)
         {
-            vocab.Keywords = vocab.Keywords.OrderBy(k => k.Value).ToList();
+            vocab.Keywords = (from k in vocab.Keywords
+                             group k by k.Value.ToLower() into g
+                             select g.First() into n
+                             orderby n.Value
+                             select n).ToList();
 
             var validation = validator.Validate(vocab);
 
@@ -112,27 +118,13 @@ namespace Catalogue.Data.Write
         public bool Success { get { return !Validation.Errors.Any(); } }
     }
 
+    [TestFixture]
     public class when_upserting_a_vocabulary
     {
-        [Test]
-        public void should_validate_and_store_record_in_the_database()
-        {
-            var database = new Mock<IDocumentSession>();
-            //mock existing vocab test
-            database.Setup(x => x.Load<Vocabulary>()).Returns<Vocabulary>(null);
 
-            var validator = new Mock<IVocabularyValidator>();
-            validator.Setup(x => x.Validate(It.IsAny<Vocabulary>())).Returns(new ValidationResult<Vocabulary>());
-
-            var service = new VocabularyService(database.Object, validator.Object);
-
-            var record = BasicVocabulary();
-            service.Upsert(record);
-
-            Mock.Get(validator.Object).Verify(v => v.Validate(record));
-            Mock.Get(database.Object).Verify(db => db.Store(record));
-        }
-
+        private Mock<IDocumentSession> database;
+        private Mock<IVocabularyValidator> validator;
+        private VocabularyService service;
 
         Vocabulary BasicVocabulary()
         {
@@ -143,6 +135,79 @@ namespace Catalogue.Data.Write
                 Keywords = new List<VocabularyKeyword>()
             };
         }
+
+
+        [SetUp]
+        public void Init()
+        {
+            database = new Mock<IDocumentSession>();
+            //mock existing vocab test
+            database.Setup(x => x.Load<Vocabulary>()).Returns<Vocabulary>(null);
+
+            validator = new Mock<IVocabularyValidator>();
+            validator.Setup(x => x.Validate(It.IsAny<Vocabulary>())).Returns(new ValidationResult<Vocabulary>());
+
+            service = new VocabularyService(database.Object, validator.Object);
+        }
+
+        [Test]
+        public void should_validate_and_store_record_in_the_database()
+        {
+            var record = BasicVocabulary();
+            service.Upsert(record);
+
+            Mock.Get(validator.Object).Verify(v => v.Validate(record));
+            Mock.Get(database.Object).Verify(db => db.Store(record));
+        }
+
+
+        [Test]
+        public void should_not_duplicate_keywords()
+        {
+            var record = BasicVocabulary();
+            record.Keywords = new List<VocabularyKeyword>
+            {
+                new VocabularyKeyword() {Value = "TestA"},
+                new VocabularyKeyword() {Value = "TestDupe"},
+                new VocabularyKeyword() {Value = "TestDupe"}
+            };
+
+            Vocabulary savedVocab = null;
+
+            database.Setup(x => x.Store(It.IsAny<Vocabulary>())).Callback<Object>((v) => savedVocab = (Vocabulary)v);
+
+            service.Upsert(record);
+
+            Assert.That(savedVocab.Keywords.Count, Is.EqualTo(2));
+            Assert.That(savedVocab.Keywords.Any(x => x.Value == "TestA"), Is.EqualTo(true));
+            Assert.That(savedVocab.Keywords.Any(x => x.Value == "TestDupe"), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void should_order_keywords_alphabetically()
+        {
+            var record = BasicVocabulary();
+            record.Keywords = new List<VocabularyKeyword>
+            {
+                new VocabularyKeyword() {Value = "Z"},
+                new VocabularyKeyword() {Value = "C"},
+                new VocabularyKeyword() {Value = "A"}
+            };
+
+            Vocabulary savedVocab = null;
+
+            database.Setup(x => x.Store(It.IsAny<Vocabulary>())).Callback<Object>((v) => savedVocab = (Vocabulary)v);
+
+            service.Upsert(record);
+
+            Assert.That(savedVocab.Keywords.Count, Is.EqualTo(3));
+            Assert.That(savedVocab.Keywords[0].Value, Is.EqualTo("A"));
+            Assert.That(savedVocab.Keywords[1].Value, Is.EqualTo("C"));
+            Assert.That(savedVocab.Keywords[2].Value, Is.EqualTo("Z"));
+        }
+
+
+
     }
 
 }
