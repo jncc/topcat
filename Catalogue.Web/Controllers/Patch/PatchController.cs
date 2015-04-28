3f0a143e34d924208c23213902d491dd2b262064
Pete Montgomery <sumo.the.cat@gmail.com>
2015-04-28 15:45:36 +0100

    patch mpas

--- Catalogue.Web/Controllers/Patch/PatchController.cs
+++ Catalogue.Web/Controllers/Patch/PatchController.cs
@@ -25,31 +25,32 @@ namespace Catalogue.Web.Controllers.Patch
         [HttpPost, Route("api/patch/fixupkeywords")]
         public HttpResponseMessage FixUpKeywords()
         {
-            // mesh 
-            var query1 = new RecordQueryInputModel
-                {
-                    K = new [] { "vocab.jncc.gov.uk/jncc-broad-category/Seabed Habitat Maps" },
-                    P = 0,
-                    N = 1024,
-                };
+//            // mesh 
+//            var query1 = new RecordQueryInputModel
+//                {
+//                    K = new [] { "vocab.jncc.gov.uk/jncc-broad-category/Seabed Habitat Maps" },
+//                    P = 0,
+//                    N = 1024,
+//                };
+//
+//            var records1 = queryer.RecordQuery(query1);
+//
+//            foreach (var record in records1)
+//            {
+//                var existing = record.Gemini.Keywords.Single(k => k.Vocab == "http://vocab.jncc.gov.uk/jncc-broad-category" && k.Value == "Seabed Habitat Maps");
+//                record.Gemini.Keywords.Remove(existing);
+//
+//                var domain = new MetadataKeyword { Vocab = "http://vocab.jncc.gov.uk/jncc-domain", Value = "Marine" };
+//                var category = new MetadataKeyword { Vocab = "http://vocab.jncc.gov.uk/jncc-category", Value = "Seabed Habitat Maps" };
+//
+//                record.Gemini.Keywords.Insert(0, domain);
+//                record.Gemini.Keywords.Insert(1, category);
+//            }
 
-            var records1 = queryer.RecordQuery(query1);
-
-            foreach (var record in records1)
-            {
-                var existing = record.Gemini.Keywords.Single(k => k.Vocab == "http://vocab.jncc.gov.uk/jncc-broad-category" && k.Value == "Seabed Habitat Maps");
-                record.Gemini.Keywords.Remove(existing);
-
-                var domain = new MetadataKeyword { Vocab = "http://vocab.jncc.gov.uk/jncc-domain", Value = "Marine" };
-                var category = new MetadataKeyword { Vocab = "http://vocab.jncc.gov.uk/jncc-category", Value = "Seabed Habitat Maps" };
-
-                record.Gemini.Keywords.Insert(0, domain);
-                record.Gemini.Keywords.Insert(1, category);
-            }
-
+            // protected sites
             var query2 = new RecordQueryInputModel
             {
-                K = new[] { "vocab.jncc.gov.uk/jncc-broad-category/Marine Protected Areas" },
+                K = new[] { "vocab.jncc.gov.uk/jncc-broad-category/Marine Protected Sites" },
                 P = 0,
                 N = 1024,
             };
