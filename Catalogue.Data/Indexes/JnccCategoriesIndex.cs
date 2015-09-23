fd3d7898eb94e2db7445cd1119cdab89dc64948e
Pete Montgomery <sumo.the.cat@gmail.com>
2015-09-23 10:47:41 +0100

    jncc categories query

---
+++ Catalogue.Data/Indexes/JnccCategoriesIndex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalogue.Data.Model;
+using Raven.Client.Indexes;
+
+namespace Catalogue.Data.Indexes
+{
+    public class JnccCategoriesIndex : AbstractIndexCreationTask<Record, JnccCategoriesIndex.Result>
+    {
+        public class Result
+        {
+            public string Category { get; set; }
+            public int Count { get; set; }
+        }
+
+        public JnccCategoriesIndex()
+        {
+            Map = records => from record in records
+                             from keyword in record.Gemini.Keywords
+                             where keyword.Vocab == "http://vocab.jncc.gov.uk/jncc-category"
+                             select new
+                             {
+                                 // todo need to account for records with multiple categories
+                                 Category = keyword.Value,
+                                 Count = 1
+                             };
+
+            Reduce = results => from result in results
+                                group result by result.Category into g
+                                select new
+                                {
+                                    Category = g.Key,
+                                    Count = g.Sum(r => r.Count)
+                                };
+        }
+    }
+}
---
+++ Catalogue.Tests/Slow/Catalogue.Data/Indexes/jncc_categories_index_spec.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalogue.Data.Indexes;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Catalogue.Tests.Slow.Catalogue.Data.Indexes
+{
+    class jncc_categories_index_spec : DatabaseTestFixture
+    {
+        [Test]
+        public void blah()
+        {
+            var results = Db.Query<JnccCategoriesIndex.Result, JnccCategoriesIndex>().ToList();
+            results.Should().NotBeEmpty();
+        }
+    }
+}
