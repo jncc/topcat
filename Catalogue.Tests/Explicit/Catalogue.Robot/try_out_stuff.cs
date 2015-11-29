3b234f90269d1cd57a542c8544483790a062c1a5
Pete Montgomery <sumo.the.cat@gmail.com>
2015-11-29 20:59:39 +0000

    seed record for pubilsh test

---
+++ Catalogue.Tests/Explicit/Catalogue.Robot/try_out_stuff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Catalogue.Tests.Explicit.Catalogue.Robot
+{
+    class try_out_stuff
+    {
+        [Explicit, Test]
+        public void blah()
+        {
+            string html = "<html><body></body></html> ";
+            var doc = XElement.Parse(html);
+            var body = doc.Element("body");
+            body.Add(new XElement("a", new XAttribute("href", "some-topcat-record.xml")));
+
+            body.Elements().Count().Should().Be(1);
+        }
+    }
+}
