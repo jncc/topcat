3de749d512739bc41c2c03c6e4869fb2c8778752
Pete Montgomery <sumo.the.cat@gmail.com>
2016-02-03 20:58:00 +0000

    new map-reduce index for counting records w duplicate titles

---
+++ Catalogue.Data/Indexes/RecordsWithDuplicateTitleCheckerIndex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalogue.Data.Model;
+using Raven.Client.Indexes;
+
+namespace Catalogue.Data.Indexes
+{
+    public class RecordsWithDuplicateTitleCheckerIndex : AbstractIndexCreationTask<Record, RecordsWithDuplicateTitleCheckerIndex.Result>
+    {
+        public class Result
+        {
+             public string Title { get; set; }
+             public int Count { get; set; }
+        }
+
+        public RecordsWithDuplicateTitleCheckerIndex()
+        {
+            Map = records => from r in records
+                             select new
+                             {
+                                 Title = r.Gemini.Title,
+                                 Count = 1
+                             };
+            Reduce = results => from r in results
+                                group r by r.Title into g
+                                select new
+                                {
+                                    Title = g.Key,
+                                    Count = g.Sum(r => r.Count)
+                                };
+        }
+    }
+}
