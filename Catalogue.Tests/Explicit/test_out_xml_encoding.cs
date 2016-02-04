14e0746cd8f61a8e7c2e112744a6a6cdd18e6919
Pete Montgomery <sumo.the.cat@gmail.com>
2016-02-04 09:58:57 +0000

    repro xml encoding issue

---
+++ Catalogue.Tests/Explicit/test_out_xml_encoding.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Catalogue.Tests.Explicit
+{
+    class test_out_xml_encoding
+    {
+        [Explicit, Test]
+        public void reproduce_xml_enocding_issue()
+        {
+            string xml = @"<some-element>© JNCC</some-element>";
+
+            new XDocument(xml).ToString().Should().Contain("&copy;");
+        }
+    }
+}
