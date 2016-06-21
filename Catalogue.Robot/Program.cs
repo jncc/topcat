c0228ae5aba457d9c3fb8c816e73deaeefe9dc5b
Pete Montgomery <sumo.the.cat@gmail.com>
2016-06-21 07:55:53 +0100

    temp skip 128

--- Catalogue.Robot/Program.cs
+++ Catalogue.Robot/Program.cs
@@ -219,7 +219,8 @@ namespace Catalogue.Robot
                     ids = query
                         .Where(x => x.GeminiValidated) // all open data should be gemini-valid - this is a safety
                         .OfType<Record>() //.Select(r => r.Id) // this doesn't work in RavenDB, and doesn't throw! so take 1000 which is enough for one run
-                        .Skip(1000)
+                        .Skip(1128)
+                        .Take(1000)
                         .ToList() // so materialize the record first
                         .Where(r => !r.Publication.OpenData.Paused) //  .Where(x => !x.PublishingIsPaused) on the server doesn't work on live - thanks, ravenDB
                         .Select(r => r.Id)
