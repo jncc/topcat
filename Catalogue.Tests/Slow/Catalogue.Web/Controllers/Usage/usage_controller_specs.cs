c526c96630de65a747a2ddf8e5b55e5096b8142c
Cathy.Jin <Cathy.Jin@jncc.gov.uk>
2017-07-06 17:21:24 +0100

    Adding new UsageController and output model for getting recently modified records, removing versioning test endpoint

---
+++ Catalogue.Tests/Slow/Catalogue.Web/Controllers/Usage/usage_controller_specs.cs
@@ -0,0 +1,38 @@
+﻿using Catalogue.Web.Controllers.Usage;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace Catalogue.Tests.Slow.Catalogue.Web.Controllers.Usage
+{
+    public class usage_controller_specs : DatabaseTestFixture
+    {
+        [Test]
+        public void recently_modified_records()
+        {
+            var usageController = new UsageController(Db);
+            var timeToTest = new DateTime(2017, 07, 12, 17, 00, 00);
+            var usageResult = usageController.GetRecentlyModifiedRecords(timeToTest);
+            var recentlyModifiedRecords = usageResult.RecentlyModifiedRecords;
+
+            recentlyModifiedRecords.Count.Should().Be(5);
+            recentlyModifiedRecords[0].Gemini.Title.Should().Be("DateTest_7");
+            recentlyModifiedRecords[1].Gemini.Title.Should().Be("DateTest_5");
+            recentlyModifiedRecords[2].Gemini.Title.Should().Be("DateTest_4");
+            recentlyModifiedRecords[3].Gemini.Title.Should().Be("DateTest_3");
+            recentlyModifiedRecords[4].Gemini.Title.Should().Be("DateTest_2");
+
+            recentlyModifiedRecords[0].Gemini.MetadataDate.Should().Be(new DateTime(2017, 07, 12, 15, 00, 00));
+            recentlyModifiedRecords[1].Gemini.MetadataDate.Should().Be(new DateTime(2017, 07, 11, 10, 00, 00));
+            recentlyModifiedRecords[2].Gemini.MetadataDate.Should().Be(new DateTime(2017, 07, 01, 14, 00, 00));
+            recentlyModifiedRecords[3].Gemini.MetadataDate.Should().Be(new DateTime(2017, 07, 01, 15, 00, 00));
+            recentlyModifiedRecords[4].Gemini.MetadataDate.Should().Be(new DateTime(2017, 07, 05, 09, 00, 00));
+
+            recentlyModifiedRecords[0].Gemini.MetadataPointOfContact.Should().Be("Cathy");
+            recentlyModifiedRecords[1].Gemini.MetadataPointOfContact.Should().Be("Pete");
+            recentlyModifiedRecords[2].Gemini.MetadataPointOfContact.Should().Be("Pete");
+            recentlyModifiedRecords[3].Gemini.MetadataPointOfContact.Should().Be("Cathy");
+            recentlyModifiedRecords[4].Gemini.MetadataPointOfContact.Should().Be("Pete");
+        }
+    }
+}
