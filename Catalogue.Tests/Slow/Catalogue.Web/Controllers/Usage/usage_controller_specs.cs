21319a9dbc4d7bb317610ecd7b9e773b1218e7f4
Cathy.Jin <Cathy.Jin@jncc.gov.uk>
2017-07-10 15:17:25 +0100

    Some refactoring of unit test

--- Catalogue.Tests/Slow/Catalogue.Web/Controllers/Usage/usage_controller_specs.cs
+++ Catalogue.Tests/Slow/Catalogue.Web/Controllers/Usage/usage_controller_specs.cs
@@ -1,17 +1,15 @@
-﻿using Catalogue.Web.Controllers.Usage;
-using FluentAssertions;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Catalogue.Data.Model;
+﻿using Catalogue.Data.Model;
 using Catalogue.Data.Test;
 using Catalogue.Data.Write;
 using Catalogue.Gemini.Model;
 using Catalogue.Gemini.Templates;
 using Catalogue.Utilities.Clone;
 using Catalogue.Utilities.Time;
-using Raven.Client;
+using Catalogue.Web.Controllers.Usage;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace Catalogue.Tests.Slow.Catalogue.Web.Controllers.Usage
 {
@@ -21,48 +19,62 @@ namespace Catalogue.Tests.Slow.Catalogue.Web.Controllers.Usage
         [Test]
         public void recently_modified_records()
         {
-            var dateTest7 = new ModifiedRecord();
-            dateTest7.Id = new Guid("1458dfd1-e356-4287-9190-65e5f9ffd1df");
-            dateTest7.Title = "DateTest_7";
-            dateTest7.Date = new DateTime(2017, 07, 12, 15, 00, 00);
-            dateTest7.User = "Cathy";
+            var dateTest7 = new ModifiedRecord
+            {
+                Id = new Guid("1458dfd1-e356-4287-9190-65e5f9ffd1df"),
+                Title = "DateTest_7",
+                Date = new DateTime(2017, 07, 12, 15, 00, 00),
+                User = "Cathy"
+            };
 
-            var dateTest5 = new ModifiedRecord();
-            dateTest5.Id = new Guid("80de0c30-325a-4392-ab4e-64b0654ca6ec");
-            dateTest5.Title = "DateTest_5";
-            dateTest5.Date = new DateTime(2017, 07, 11, 10, 00, 00);
-            dateTest5.User = "Pete";
+            var dateTest5 = new ModifiedRecord
+            {
+                Id = new Guid("80de0c30-325a-4392-ab4e-64b0654ca6ec"),
+                Title = "DateTest_5",
+                Date = new DateTime(2017, 07, 11, 10, 00, 00),
+                User = "Pete"
+            };
 
-            var dateTest4 = new ModifiedRecord();
-            dateTest4.Id = new Guid("f5a48ac7-13f6-40ba-85a2-f4534d9806a5");
-            dateTest4.Title = "DateTest_4";
-            dateTest4.Date = new DateTime(2017, 07, 01, 15, 05, 0);
-            dateTest4.User = "Pete";
+            var dateTest4 = new ModifiedRecord
+            {
+                Id = new Guid("f5a48ac7-13f6-40ba-85a2-f4534d9806a5"),
+                Title = "DateTest_4",
+                Date = new DateTime(2017, 07, 01, 15, 05, 0),
+                User = "Pete"
+            };
 
-            var dateTest3 = new ModifiedRecord();
-            dateTest3.Id = new Guid("8c88dd97-3317-43e4-b59e-239e0604a094");
-            dateTest3.Title = "DateTest_3";
-            dateTest3.Date = new DateTime(2017, 07, 01, 15, 00, 00);
-            dateTest3.User = "Cathy";
+            var dateTest3 = new ModifiedRecord
+            {
+                Id = new Guid("8c88dd97-3317-43e4-b59e-239e0604a094"),
+                Title = "DateTest_3",
+                Date = new DateTime(2017, 07, 01, 15, 00, 00),
+                User = "Cathy"
+            };
 
-            var dateTest2 = new ModifiedRecord();
-            dateTest2.Id = new Guid("3ad98517-110b-40d7-aa0d-f0e3b1273007");
-            dateTest2.Title = "DateTest_2";
-            dateTest2.Date = new DateTime(2017, 06, 05, 09, 00, 00);
-            dateTest2.User = "Pete";
+            var dateTest2 = new ModifiedRecord
+            {
+                Id = new Guid("3ad98517-110b-40d7-aa0d-f0e3b1273007"),
+                Title = "DateTest_2",
+                Date = new DateTime(2017, 06, 05, 09, 00, 00),
+                User = "Pete"
+            };
 
-            var expectedList = new List<ModifiedRecord>();
-            expectedList.Add(dateTest7);
-            expectedList.Add(dateTest5);
-            expectedList.Add(dateTest4);
-            expectedList.Add(dateTest3);
-            expectedList.Add(dateTest2);
+            var dateTest1 = new ModifiedRecord
+            {
+                Id = new Guid("8f4562ea-9d8a-45a0-afd3-bc5072d342a0"),
+                Title = "DateTest_1",
+                Date = new DateTime(2016, 01, 03, 09, 00, 00),
+                User = "Cathy"
+            };
 
-            IDocumentStore store = new InMemoryDatabaseHelper().Create();
-            using (IDocumentSession db = store.OpenSession())
+            var testRecords = new List<ModifiedRecord> { dateTest7, dateTest5, dateTest4, dateTest3, dateTest2, dateTest1 };
+            var expectedList = new List<ModifiedRecord> { dateTest7, dateTest5, dateTest4, dateTest3, dateTest2 };
+
+            var store = new InMemoryDatabaseHelper().Create();
+            using (var db = store.OpenSession())
             {
                 IRecordService recordService = new RecordService(db, new RecordValidator());
-                AddLastModifiedDateRecords(recordService);
+                AddLastModifiedDateRecords(testRecords, recordService);
                 db.SaveChanges();
 
                 var usageController = new UsageController(db);
@@ -75,37 +87,21 @@ namespace Catalogue.Tests.Slow.Catalogue.Web.Controllers.Usage
             }
         }
 
-        void AddLastModifiedDateRecords(IRecordService recordService)
+        private static void AddLastModifiedDateRecords(List<ModifiedRecord> testRecords, IRecordService recordService)
         {
-            var tuples = new[]
+            foreach (var testRecord in testRecords)
             {
-                Tuple.Create("8f4562ea-9d8a-45a0-afd3-bc5072d342a0", "DateTest_1",
-                    new DateTime(2016, 01, 03, 09, 00, 00), "Cathy"),
-                Tuple.Create("3ad98517-110b-40d7-aa0d-f0e3b1273007", "DateTest_2",
-                    new DateTime(2017, 06, 05, 09, 00, 00), "Pete"),
-                Tuple.Create("8c88dd97-3317-43e4-b59e-239e0604a094", "DateTest_3",
-                    new DateTime(2017, 07, 01, 15, 00, 00), "Cathy"),
-                Tuple.Create("f5a48ac7-13f6-40ba-85a2-f4534d9806a5", "DateTest_4",
-                    new DateTime(2017, 07, 01, 15, 05, 00), "Pete"),
-                Tuple.Create("80de0c30-325a-4392-ab4e-64b0654ca6ec", "DateTest_5",
-                    new DateTime(2017, 07, 11, 10, 00, 00), "Pete"),
-                Tuple.Create("1458dfd1-e356-4287-9190-65e5f9ffd1df", "DateTest_7",
-                    new DateTime(2017, 07, 12, 15, 00, 00), "Cathy"),
-            };
-
-            foreach (Tuple<string, string, DateTime, string> tuple in tuples)
-            {
-                Clock.CurrentUtcDateTimeGetter = () => tuple.Item3;
+                Clock.CurrentUtcDateTimeGetter = () => testRecord.Date;
 
                 var record = MakeExampleSeedRecord().With(r =>
                 {
-                    r.Id = new Guid(tuple.Item1);
-                    r.Path = @"X:\path\to\last\modified\date\data\" + tuple.Item2;
+                    r.Id = testRecord.Id;
+                    r.Path = @"X:\path\to\last\modified\date\data\" + testRecord.Title;
                     r.Gemini = r.Gemini.With(m =>
                     {
-                        m.Title = tuple.Item2;
+                        m.Title = testRecord.Title;
                         m.Abstract = "Record with different last modified date";
-                        m.MetadataPointOfContact.Name = tuple.Item4;
+                        m.MetadataPointOfContact.Name = testRecord.User;
                     });
                 });
 
@@ -113,7 +109,7 @@ namespace Catalogue.Tests.Slow.Catalogue.Web.Controllers.Usage
             }
         }
 
-        Record MakeExampleSeedRecord()
+        private static Record MakeExampleSeedRecord()
         {
             return new Record
             {
