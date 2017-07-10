48502dc2028fddb7934faf53b5bc3cbe9aa89b1b
Cathy.Jin <Cathy.Jin@jncc.gov.uk>
2017-07-10 12:46:25 +0100

    Changing timeline test to use separate db instance

--- Catalogue.Web/Controllers/Usage/UsageOutputModel.cs
+++ Catalogue.Web/Controllers/Usage/UsageOutputModel.cs
@@ -15,5 +15,25 @@ namespace Catalogue.Web.Controllers.Usage
         public string Title { get; set; }
         public DateTime Date { get; set; }
         public string User { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as ModifiedRecord;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return Id.Equals(item.Id)
+                   && Title.Equals(item.Title)
+                   && Date.Equals(item.Date)
+                   && User.Equals(item.User);
+        }
+
+        public override int GetHashCode()
+        {
+            return new {Id, Title, Date, User}.GetHashCode();
+        }
     }
 }
