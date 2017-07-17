54ff6faa5c17f04269451c8f967c0980fe8ac3dd
Cathy.Jin <Cathy.Jin@jncc.gov.uk>
2017-07-17 10:27:27 +0100

    Added new endpoint for marking, starting unit test

---
+++ Catalogue.Tests/Slow/Catalogue.Web/Controllers/Marking/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Catalogue.Web.Controllers.Marking;
+using NUnit.Framework;
+
+namespace Catalogue.Tests.Slow.Catalogue.Web.Controllers.Marking
+{
+    class marking_specs : DatabaseTestFixture
+    {
+        [Test]
+        public void marking_security_group_test()
+        {
+            var markingController = new OpenDataMarkingController(Db);
+        }
+    }
+}
---
+++ Catalogue.Web/Controllers/Marking/OpenDataMarkingController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Web.Http;
+using Catalogue.Data.Write;
+using Raven.Client;
+using Catalogue.Data.Model;
+using Catalogue.Gemini.Model;
+
+namespace Catalogue.Web.Controllers.Marking
+{
+    public class OpenDataMarkingController : ApiController
+    {
+        readonly IDocumentSession db;
+
+        public OpenDataMarkingController(IDocumentSession db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet, Route("api/marking/opendata")]
+        public RecordServiceResult MarkAsOpenData(Guid id)
+        {
+            var recordServiceResult = new RecordServiceResult();
+            var record = db.Load<Record>(id);
+
+            if (record.Publication == null)
+                record.Publication = new PublicationInfo();
+
+            if (record.Publication.OpenData == null)
+            {
+                record.Publication.OpenData = new OpenDataPublicationInfo();
+            }
+                
+            db.SaveChanges();
+
+            return recordServiceResult;
+        }
+    }
+}
