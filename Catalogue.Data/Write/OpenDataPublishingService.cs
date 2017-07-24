33f8d2faf43e3569434ab4b3f49a3818bb2a4b47
Cathy@JNCC <Cathy.Jin@jncc.gov.uk>
2017-07-24 15:50:06 +0100

    Changes for comments

--- Catalogue.Data/Write/OpenDataPublishingService.cs
+++ Catalogue.Data/Write/OpenDataPublishingService.cs
@@ -18,21 +18,27 @@ namespace Catalogue.Data.Write
         public void SignOff(Record record, OpenDataSignOffInfo signOffInfo)
         {
             if (record.Publication?.OpenData == null)
-                throw new Exception("OpenDataAssessmentInfo not completed");
+                throw new Exception("Couldn't sign-off record for publication. Assessment not completed.");
 
             var openDataInfo = record.Publication.OpenData;
 
             if (openDataInfo?.SignOff != null && openDataInfo.SignOff.DateUtc != DateTime.MinValue)
-                throw new Exception("Record already signed off");
+                throw new Exception("The record has already been signed off and cannot be signed off again.");
 
             if (openDataInfo?.Assessment != null && !openDataInfo.Assessment.Completed)
-                throw new Exception("OpenDataAssessmentInfo not completed");
+                throw new Exception("Couldn't sign-off record for publication. Assessment not completed.");
 
             openDataInfo.SignOff = signOffInfo;
 
             var recordServiceResult = recordService.Update(record);
             if (recordServiceResult.Success)
+            {
                 db.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Error while saving sign off changes.");
+            }
         }
     }
 }
