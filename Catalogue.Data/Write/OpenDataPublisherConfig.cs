a290c6dbc6a4a75053e72028060bd61038cbaf29
Cathy@JNCC <Cathy.Jin@jncc.gov.uk>
2017-08-18 17:24:30 +0100

    First pass at refactoring the upload functionality

---
+++ Catalogue.Data/Write/OpenDataPublisherConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalogue.Data.Write
+{
+    public class OpenDataPublisherConfig
+    {
+        public string HttpRootUrl { get; set; }
+        public string FtpRootUrl { get; set; }
+        public string FtpUsername { get; set; }
+        public string FtpPassword { get; set; }
+    }
+}
--- Catalogue.Data/Write/OpenDataPublishingService.cs
+++ Catalogue.Data/Write/OpenDataPublishingService.cs
@@ -1,6 +1,16 @@
-﻿using System;
-using Catalogue.Data.Model;
+﻿using Catalogue.Data.Model;
+using Catalogue.Gemini.Model;
+using Catalogue.Utilities.Text;
+using Catalogue.Utilities.Time;
 using Raven.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
+using Catalogue.Robot.Publishing.OpenData;
+using static Catalogue.Utilities.Text.WebificationUtility;
 
 namespace Catalogue.Data.Write
 {
@@ -8,11 +18,13 @@ namespace Catalogue.Data.Write
     {
         private readonly IDocumentSession db;
         private readonly IRecordService recordService;
+        private readonly IOpenDataUploadService uploadService;
 
-        public OpenDataPublishingService(IDocumentSession db, IRecordService recordService)
+        public OpenDataPublishingService(IDocumentSession db, IRecordService recordService, IOpenDataUploadService uploadService)
         {
             this.db = db;
             this.recordService = recordService;
+            this.uploadService = uploadService;
         }
 
         public void SignOff(Record record, OpenDataSignOffInfo signOffInfo)
@@ -78,6 +90,94 @@ namespace Catalogue.Data.Write
             db.SaveChanges();
 
             return recordServiceResult.Record;
+        }
+
+        public void Upload(Record record, UserInfo userInfo, bool metadataOnly)
+        {
+            var attempt = new PublicationAttempt { DateUtc = Clock.NowUtc };
+            UpdateLastAttempt(record, attempt, userInfo);
+
+            bool alternativeResources = record.Publication != null && record.Publication.OpenData != null && record.Publication.OpenData.Resources != null && record.Publication.OpenData.Resources.Any();
+            bool corpulent = record.Gemini.Keywords.Any(k => k.Vocab == "http://vocab.jncc.gov.uk/metadata-admin" && k.Value == "Corpulent");
+
+            try
+            {
+                if (alternativeResources)
+                {
+                    // upload the alternative resources; don't touch the resource locator
+                    uploadService.UploadAlternativeResources(record, metadataOnly);
+                }
+                else
+                {
+                    if (corpulent)
+                    {
+                        // set the resource locator to the download request page; don't upload any resources
+                        if (record.Gemini.ResourceLocator.IsBlank()) // arguably should always do it actually
+                            UpdateTheResourceLocatorToBeTheOpenDataDownloadPage(record);
+                    }
+                    else if (record.Gemini.ResourceLocator.IsBlank() || record.Gemini.ResourceLocator.Contains(uploadService.GetHttpRootUrl()))
+                    {
+                        // "normal" case - if the resource locator is blank or already data.jncc.gov.uk
+                        // upload the resource pointed at by record.Path, and update the resource locator to match
+                        uploadService.UploadDataFile(record.Id, record.Path, metadataOnly);
+                        UpdateResourceLocatorToMatchMainDataFile(record);
+                    }
+                    else
+                    {
+                        // do nothing - don't change the resource locator, don't upload anything
+                        Console.WriteLine("Deferring to existing resource locator - not uploading.");
+                    }
+                }
+                
+                uploadService.UploadMetadataDocument(record);
+                uploadService.UploadWafIndexDocument(record);
+
+                record.Publication.OpenData.LastSuccess = attempt;
+            }
+            catch (WebException ex)
+            {
+                string message = ex.Message + (ex.InnerException != null ? ex.InnerException.Message : "");
+                Console.WriteLine(message);
+                attempt.Message = message;
+            }
+
+            // commit the changes - to both the record (resource locator may have changed) and the attempt object
+            db.SaveChanges();
+        }
+
+        public List<Record> GetRecordsPendingUpload()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateLastAttempt(Record record, PublicationAttempt attempt, UserInfo userInfo)
+        {
+            // save a not-yet-successful attempt to begin with
+            record.Publication.OpenData.LastAttempt = attempt;
+
+            var recordServiceResult = recordService.Update(record, userInfo);
+            if (!recordServiceResult.Success)
+            {
+                throw new Exception("Error while saving upload changes.");
+            }
+
+            db.SaveChanges();
+        }
+
+        void UpdateTheResourceLocatorToBeTheOpenDataDownloadPage(Record record)
+        {
+            // this is a big dataset so just link to a webpage
+            string jnccWebDownloadPage = "http://jncc.defra.gov.uk/opendata";
+            record.Gemini.ResourceLocator = jnccWebDownloadPage;
+            Console.WriteLine("ResourceLocator updated to point to open data request webpage.");
+        }
+
+        void UpdateResourceLocatorToMatchMainDataFile(Record record)
+        {
+            // update the resource locator to be the data file
+            string dataHttpPath = uploadService.GetHttpRootUrl() + "/" + GetUnrootedDataPath(record.Id, record.Path);
+            record.Gemini.ResourceLocator = dataHttpPath;
+            Console.WriteLine("ResourceLocator updated to point to the data file.");
         }
     }
 }
