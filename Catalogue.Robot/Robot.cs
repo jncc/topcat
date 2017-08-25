1f8c9bd25b92605d72050b88b1fca800ab296b6c
Cathy@JNCC <Cathy.Jin@jncc.gov.uk>
2017-08-25 12:33:17 +0100

    Testing connection

--- Catalogue.Robot/Robot.cs
+++ Catalogue.Robot/Robot.cs
@@ -1,7 +1,11 @@
 ﻿using Catalogue.Data.Model;
 using Raven.Client;
 using System;
+using System.IO;
 using System.Linq;
+using Catalogue.Robot.Publishing.OpenData;
+using Catalogue.Utilities.Text;
+using Newtonsoft.Json;
 
 namespace Catalogue.Robot
 {
@@ -24,6 +28,24 @@ namespace Catalogue.Robot
             {
                 var record = db.Query<Record>().First(r => r.Gemini.Title.StartsWith("sea"));
                 Console.WriteLine(record.Gemini.Title);
+
+                var configPath = Path.Combine(Environment.CurrentDirectory, "data-gov-uk-publisher-config.json");
+                if (!File.Exists(configPath))
+                    throw new Exception("No data-gov-uk-publisher-config.json file in current directory.");
+                string configJson = File.ReadAllText(configPath);
+                var config = JsonConvert.DeserializeObject<OpenDataPublisherConfig>(configJson);
+                if (config.FtpRootUrl.IsBlank())
+                    throw new Exception("No FtpRootUrl specified in data-gov-uk-publisher-config.json file.");
+                if (config.HttpRootUrl.IsBlank())
+                    throw new Exception("No HttpRootUrl specified in data-gov-uk-publisher-config.json file.");
+                if (config.FtpUsername.IsBlank())
+                    throw new Exception("No FtpUsername specified in data-gov-uk-publisher-config.json file.");
+                if (config.FtpPassword.IsBlank())
+                    throw new Exception("No FtpPassword specified in data-gov-uk-publisher-config.json file.");
+
+                var ftpClient = new FtpClient(config.FtpUsername, config.FtpPassword);
+                new OpenDataRecordPublisher(db, config, true, ftpClient).PublishRecord(record.Id);
+                Console.WriteLine("Finished");
             }
         }
 
